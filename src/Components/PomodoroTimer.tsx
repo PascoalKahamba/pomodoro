5653f8024ca8bca410b5397db882cb781fa9e136
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-01-07 22:05:20 +0100

    created the states timeCounting and working and function configure to start the states

--- src/Components/PomodoroTimer.tsx
+++ src/Components/PomodoroTimer.tsx
@@ -11,17 +11,33 @@ interface Props {
 
 const PomodoroTimer = ({ PromodoroTime }: Props) => {
   const [mainTime, setMainTime] = useState(PromodoroTime);
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null
+  );
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
+
   return (
     <div className="pomodoro">
       <h2>You are working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
+        <Button text="work" onClick={() => configureWork()} />
         <Button text="texto" onClick={() => console.log(1)} />
         <Button text="texto" onClick={() => console.log(1)} />
-        <Button text="texto" onClick={() => console.log(1)} />
+      </div>
+
+      <div className="details">
+        <p>Exemple of the Details</p>
+        <p>Exemple of the Details</p>
+        <p>Exemple of the Details</p>
       </div>
     </div>
   );
